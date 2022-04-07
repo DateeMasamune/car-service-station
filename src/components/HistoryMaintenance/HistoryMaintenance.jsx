25a8d1ec956384c0e497a1b28e76c8930b5a5b12
Dmitriy Ovsienko <ovsienkodima@mail.ru>
2022-04-08 00:12:46 +0300

    add filter for car services

--- src/components/HistoryMaintenance/HistoryMaintenance.jsx
+++ src/components/HistoryMaintenance/HistoryMaintenance.jsx
@@ -5,7 +5,9 @@ import Divider from '@mui/material/Divider';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
+import {
+  Box, FormControl, MenuItem, Select,
+} from '@mui/material';
 
 import wheel from '../../assets/wheel.jpg';
 import Skeleton from '../Skeleton/Skeleton';
@@ -14,14 +16,38 @@ import useHistoryMaintenance from './useHistoryMaintenance';
 function HistoryMaintenance() {
   const {
     carPark,
+    serviceStation,
+    selectService,
     goToSingleCar,
     filterMaintenance,
+    handleChangeService,
   } = useHistoryMaintenance();
 
   return (
     <>
+      <FormControl fullWidth style={{ marginBottom: 20, minWidth: 300 }}>
+        <Select
+          value={selectService}
+          onChange={handleChangeService}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem disabled value="">
+            <em>Станция техобслуживании</em>
+          </MenuItem>
+          <MenuItem value="">Все станции</MenuItem>
+          {serviceStation.map((serviceName) => (
+            <MenuItem
+              key={serviceName.id}
+              value={serviceName.id}
+            >
+              {serviceName.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       {carPark.filter(filterMaintenance).length === 0 && (
-        <Skeleton text="Автомобили еще не проходили тех обслуживание" />
+      <Skeleton text="Автомобили еще не проходили тех обслуживание" />
       )}
       {carPark.filter(filterMaintenance).map((car) => (
         <List onClick={() => goToSingleCar(car.id)} key={car.id} sx={{ width: '100%', bgcolor: 'background.paper', cursor: 'pointer' }}>
@@ -45,7 +71,7 @@ function HistoryMaintenance() {
                 История ТО
               </Typography>
               {car.history.map((history) => (
-                <Typography style={{ color: 'gray', fontSize: '14px' }}>{`${history}`}</Typography>
+                <Typography key={history} style={{ color: 'gray', fontSize: '14px' }}>{`${history}`}</Typography>
               ))}
               {car.status === 'ready'
                 ? <Typography style={{ color: 'green' }}>Техобслуживание пройдено успешно</Typography>
--- src/components/HistoryMaintenance/useHistoryMaintenance.js
+++ src/components/HistoryMaintenance/useHistoryMaintenance.js
@@ -1,21 +1,40 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import useCurrentCar from '../../customHooks/useCurrentCar';
 
 function useHistoryMaintenance() {
+  const [selectService, setSelectService] = useState('');
   const { carPark } = useCurrentCar();
   const navigate = useNavigate();
-
+  const serviceStation = useSelector((store) => store.serviceStation);
   const goToSingleCar = (id) => {
     navigate(`/maintenance/${id}`);
   };
 
-  const filterMaintenance = (car) => (car.status !== '');
+  const handleChangeService = (event) => {
+    setSelectService(event.target.value);
+  };
+
+  const filterMaintenance = (car) => {
+    if (selectService === '') {
+      if (car.status !== '') {
+        return true;
+      }
+    } else if (car.serviceId === selectService) {
+      return true;
+    }
+    return false;
+  };
 
   return {
     carPark,
     goToSingleCar,
     filterMaintenance,
+    serviceStation,
+    handleChangeService,
+    selectService,
   };
 }
 
